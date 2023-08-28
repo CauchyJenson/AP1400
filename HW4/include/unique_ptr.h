b868d1d41d627875f4563517401a8ecf4b366c78
cauchyjenson <cauchyjenson@gmail.com>
2023-08-28 16:22:19 +0800

    unique_ptr

--- HW4/include/unique_ptr.h
+++ HW4/include/unique_ptr.h
@@ -1,5 +1,56 @@
 #ifndef UNIQUE_PTR
 #define UNIQUE_PTR
+#include<memory>
+
+template<typename T>
+class UniquePtr
+{
+private:
+    /* data */
+    T* _p = nullptr;
+    
+public:
+    UniquePtr(): _p(nullptr){}
+    UniquePtr(T* p): _p(p){}
+    UniquePtr(std::unique_ptr ptr): _p(ptr.release()){}
+    UniquePtr(UniquePtr<T>& p){};
+    UniquePtr operator=(const UniquePtr& p){};
+
+    ~UniquePtr(){
+        delete _p;
+        _p = nullptr;
+    }
+
+    T* get(){
+        return _P;
+    }
+
+    T& operator*(){
+        return *_p;
+    }
+
+    T* operator->(){
+        return _p;
+    }
+
+    void reset(){
+        delete _p;
+        _p = nullptr;
+    }
+
+    void reset(T* t){
+        delete _p;
+        _p = t;
+    }
+
+    T* release(){
+        auto tmp = _p;
+        _p = nullptr;
+        return tmp;
+    }
+
+    
+};
 
 
 
