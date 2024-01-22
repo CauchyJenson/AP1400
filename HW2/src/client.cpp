d3cc608f1649225d25b85fb15563f5cb8f0437e9
cauchyjenson <cauchyjenson@gmail.com>
2024-01-22 21:46:33 +0800

    TEST2 done

--- HW2/src/client.cpp
+++ HW2/src/client.cpp
@@ -1,1 +1,13 @@
 #include "client.h"
+
+Client::Client(std::string id, const Server& server):
+    id(id), server(&server){
+    crypto::generate_key(this->public_key, this->private_key);
+}
+
+std::string Client::get_id(){
+    return this->id;
+}
+std::string Client::get_publickey(){
+    return this->public_key;
+}
--- HW2/src/server.cpp
+++ HW2/src/server.cpp
@@ -1,1 +1,33 @@
 #include "server.h"
+
+Server::Server(){}
+
+std::shared_ptr<Client>  Server::add_client(std::string id){
+    // tail: if id exists, add 4 digits to the end
+    std::default_random_engine e;
+    std::uniform_int_distribution<int> u(0, 9);
+    std::string tail = "";
+    for(int i = 0; i < 4; ++i){
+        tail += std::to_string(u(e));
+    }
+    // search
+    for(auto it = clients.begin(); it != clients.end(); ++it){
+        if(it->first->get_id() == id){
+            id += tail;
+        }
+    }
+    // add
+    auto now_client = std::make_shared<Client>(id,*this);
+    clients.insert(std::make_pair(now_client, 5));
+    return now_client;
+}
+
+std::shared_ptr<Client> Server::get_client(std::string id){
+    for(auto it = clients.begin(); it != clients.end(); ++it){
+        if(it->first->get_id() == id){
+            return it->first;
+        }
+    }
+    return nullptr;
+}
+
