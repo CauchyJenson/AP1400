2d3771e490e3674b3ec7d6f49eba93d34818b0ef
cauchyjenson <cauchyjenson@gmail.com>
2024-02-03 20:38:10 +0800

    TEST17 done

--- HW4/include/shared_ptr.h
+++ HW4/include/shared_ptr.h
@@ -1,5 +1,79 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
 
+template <typename T>
+class SharedPtr{
+private:
+    T* _p;
+    int* _count;
+
+public:
+    SharedPtr(T* p = nullptr);
+    SharedPtr(SharedPtr& other);
+    ~SharedPtr(){
+        _p = nullptr;
+        if(_count){
+            --(*_count);
+            if(*_count == 0)
+                delete _count;
+        }
+    }
+
+    T& operator*();
+    T* operator->();
+
+    T* get();
+    int use_count();
+};
+
+
+template <typename T>
+SharedPtr<T>::SharedPtr(T* p){
+    _p = p;
+    if(_p)
+        _count = new int{1};
+    else
+        _count = new int{0};
+}
+
+template <typename T>
+SharedPtr<T>::SharedPtr(SharedPtr& other){
+    if(&other == this)return;
+    if(other._p == nullptr){
+        _p = nullptr;
+        _count = new int{0};
+        return;
+    }
+    _p = other._p;
+    _count = other._count;
+    (*_count)++;
+}
+
+template <typename T>
+T& SharedPtr<T>::operator*(){
+    return *_p;
+}
+
+template <typename T>
+T* SharedPtr<T>::operator->(){
+    return _p;
+}
+
+
+template <typename T>
+T* SharedPtr<T>::get(){
+    return _p;
+}
+
+template <typename T>
+int SharedPtr<T>::use_count(){
+    return *_count;
+}
+
+
+template <typename T>
+SharedPtr<T> make_shared(T val){
+    return SharedPtr<T>(new T{val});
+}
 
 #endif //SHARED_PTR
--- HW4/src/unit_test.cpp
+++ HW4/src/unit_test.cpp
@@ -83,7 +83,7 @@ TEST(HW4Test, TEST9) {
     EXPECT_FALSE(ptr);
     delete tmp;
 }
-/*
+
 TEST(HW4Test, TEST10) {
     SharedPtr<int> ptr1{new int{10}};
     EXPECT_EQ(*ptr1.get(), 10);
@@ -169,7 +169,7 @@ TEST(HW4Test, TEST17) {
     SharedPtr<std::vector<int>> ptr3{new std::vector<int>{1, 2, 3, 4, 5}};
     EXPECT_FALSE(ptr3->empty());
 }
-
+/*
 TEST(HW4Test, TEST18) {
     SharedPtr<std::string> ptr{new std::string{"hello world!"}};
     EXPECT_EQ(ptr->length(), 12);
